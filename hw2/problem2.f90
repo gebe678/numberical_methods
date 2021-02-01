67eb2d2cc5fe8201c414170aef5c858c168cb9a1
Griffin <lehrerg6@gmail.com>
2021-02-01 16:04:28 -0500

    finished problem 2

--- hw2/problem2.f90
+++ hw2/problem2.f90
@@ -1,5 +1,21 @@
 program problem2
     implicit none
     
+    real :: age
+    real :: carbonRemaining
+
+    write(*,*) "enter a percentage of carbon remaining"
+    write(*,*) "percentage should be between 0 and 1"
+
+    read(*,*) carbonRemaining
+
+    do while(carbonRemaining > 1 .OR. carbonRemaining < 0)
+        write(*,*) "Incorrect Input please enter a number between 0 and 1"
+        read(*,*) carbonRemaining
+    enddo
+
+    age = - log(carbonRemaining) / 0.0001216
+
+    write(*,*) "the age is ", age
 
 end program problem2
